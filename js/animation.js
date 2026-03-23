9506f5f7b0aca92bd53d4420d20313f60435099f
mturkosz <mturkosz80@uczen.tl.krakow.pl>
2026-03-23 23:34:24 +0100

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -1,16 +1,1 @@
-const swup = new Swup();
-const overlay = document.querySelector('.transition-overlay');
 
-swup.hooks.on('visit:start', () => {
-    overlay.classList.add('active');
-
-    setTimeout(() => {
-        overlay.classList.add('expand');
-    }, 300);
-});
-
-swup.hooks.on('content:replace', () => {
-    setTimeout(() => {
-        overlay.classList.remove('active', 'expand');
-    }, 300);
-});
